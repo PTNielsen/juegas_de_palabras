ad45fe2eb6feac7eff9df35e62ab92888593c14f
Joshua Szmajda <josh@optoro.com>
2011-06-10 12:30:03 -0400

    initial app layout

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.8'
+
+gem 'mysql2', '~> 0.2.6'
+
+gem 'ruby-debug'
+
+gem 'sass'
+gem 'haml'
+gem 'jquery-rails'
+
+group :development do
+  gem "annotate"
+end
+
+group :test do
+  gem "capybara"
+  gem "launchy"
+  gem "cucumber"
+  gem "cucumber-rails"
+end
+
+group :development, :test do
+  gem "rspec-rails"
+  gem "factory_girl_rails"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Madlibs::Application.config.secret_token = 'eb61272f2c326818391de86b841570a22bf7606f260ee1dec2ff7ebe6a313aca5b289ad00a67b09742b5011b481573f583f62f7a3f2d321977225d8b17146fdc'
